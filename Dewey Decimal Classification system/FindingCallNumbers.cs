2734ae7ff44cc43b95314c7916a03ceb60a1036c
ThusoVC <146183893+ThusoVC@users.noreply.github.com>
2023-11-21 11:46:16 +0200

    Add files via upload

---
+++ Dewey Decimal Classification system/FindingCallNumbers.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Dewey_Decimal_Classification_system
+{
+    public partial class FindingCallNumbers : Form
+    {
+        public string toDisplay;
+        public int score = 0;
+
+        public FindingCallNumbers()
+        {
+            InitializeComponent();
+
+            // loads the questionnair onto the form
+            loadingData();
+
+        }
+        // this module creates the questions and the answers.
+        public void loadingData()
+        {
+            List<string> options = new List<string>();
+            List<string> questions = new List<string>();
+
+            var callNumberFile = File.ReadAllLines("CallNumbers.txt");
+            foreach (var l in callNumberFile)
+            {
+                if (l.Contains("000") || l.Contains("100") || l.Contains("200") || l.Contains("300")
+                    || l.Contains("400") || l.Contains("500") || l.Contains("600") || l.Contains("700")
+                    || l.Contains("800") || l.Contains("900"))
+                {
+                    options.Add(l);
+                }
+                else if (l != "" && l != " ")
+                {
+                    questions.Add(l);
+                }
+            }
+
+            Random r = new Random();
+
+            int ind = r.Next(questions.Count);
+
+            toDisplay = questions[ind];
+            string result = toDisplay.Remove(0, 4);
+
+            questionLbl.Text = result;
+
+            for (int i = 0; i < 3; i++)
+            {
+                int rr = r.Next(options.Count);
+                comboBox1.Items.Add(options[rr]);
+            }
+            comboBox1.Items.Add(options[int.Parse(toDisplay.Substring(0, 1))]);
+        }
+        // submit button that processes the users answer.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int q;
+            int a;
+
+            string qu = toDisplay.Substring(0, 3);
+
+            string selected = comboBox1.SelectedItem.ToString();
+            string an = selected.Substring(0,3);
+
+            a = int.Parse(an);
+            q = int.Parse(qu);
+
+            int range = a + 99;
+            //checks if the answer is correct and awards point or deduct points based on the result.
+            if (a <= q && q <= range)
+            {
+                SubmitBtn.ForeColor = Color.Green;
+                MessageLbl.Text = " Awesome! You have chosen the Correct Answer.";
+                MessageLbl.ForeColor = Color.Green;
+
+                score = score + 50;
+
+                ScoreLbl.Text = score.ToString();
+            }
+            else
+            {
+                SubmitBtn.ForeColor = Color.Red;
+                MessageLbl.Text = "Oops! You have chosen the inccorect answer, try again.";
+                MessageLbl.ForeColor = Color.Red;
+
+                score = score - 50;
+
+                ScoreLbl.Text = score.ToString();
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SubmitBtn.ForeColor = Color.Black;
+            MessageLbl.ForeColor = Color.Black;
+            MessageLbl.Text = "";
+
+            comboBox1.Text = "Select the correct answer below";
+            comboBox1.Items.Clear(); 
+            loadingData();
+        }
+
+        private void FindingCallNumbers_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void QuitBtn_Click(object sender, EventArgs e)
+        {
+            TasksForm tf = new TasksForm();
+            tf.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Dewey Decimal Classification system/FindingCallNumbers.designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Dewey_Decimal_Classification_system
+{
+    partial class FindingCallNumbers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.questionLbl = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SubmitBtn = new System.Windows.Forms.Button();
+            this.NextBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MessageLbl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ScoreLbl = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.QuitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // questionLbl
+            // 
+            this.questionLbl.AutoSize = true;
+            this.questionLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionLbl.Location = new System.Drawing.Point(72, 70);
+            this.questionLbl.Name = "questionLbl";
+            this.questionLbl.Size = new System.Drawing.Size(87, 25);
+            this.questionLbl.TabIndex = 0;
+            this.questionLbl.Text = "asasas";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(77, 134);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(436, 33);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Select the correct answer below";
+            // 
+            // SubmitBtn
+            // 
+            this.SubmitBtn.Location = new System.Drawing.Point(77, 214);
+            this.SubmitBtn.Name = "SubmitBtn";
+            this.SubmitBtn.Size = new System.Drawing.Size(103, 23);
+            this.SubmitBtn.TabIndex = 2;
+            this.SubmitBtn.Text = "Submit";
+            this.SubmitBtn.UseVisualStyleBackColor = true;
+            this.SubmitBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // NextBtn
+            // 
+            this.NextBtn.Location = new System.Drawing.Point(282, 214);
+            this.NextBtn.Name = "NextBtn";
+            this.NextBtn.Size = new System.Drawing.Size(103, 23);
+            this.NextBtn.TabIndex = 3;
+            this.NextBtn.Text = "Next Question";
+            this.NextBtn.UseVisualStyleBackColor = true;
+            this.NextBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Question";
+            // 
+            // MessageLbl
+            // 
+            this.MessageLbl.AutoSize = true;
+            this.MessageLbl.Location = new System.Drawing.Point(77, 170);
+            this.MessageLbl.Name = "MessageLbl";
+            this.MessageLbl.Size = new System.Drawing.Size(0, 13);
+            this.MessageLbl.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Answer";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(528, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Score:";
+            // 
+            // ScoreLbl
+            // 
+            this.ScoreLbl.AutoSize = true;
+            this.ScoreLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLbl.ForeColor = System.Drawing.Color.SandyBrown;
+            this.ScoreLbl.Location = new System.Drawing.Point(573, 54);
+            this.ScoreLbl.Name = "ScoreLbl";
+            this.ScoreLbl.Size = new System.Drawing.Size(14, 13);
+            this.ScoreLbl.TabIndex = 8;
+            this.ScoreLbl.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(643, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Answer the question below. Points will be awarded for every correct answer, howev" +
+    "er point will be deducted for every incorrect answer.";
+            // 
+            // QuitBtn
+            // 
+            this.QuitBtn.Location = new System.Drawing.Point(13, 410);
+            this.QuitBtn.Name = "QuitBtn";
+            this.QuitBtn.Size = new System.Drawing.Size(75, 23);
+            this.QuitBtn.TabIndex = 10;
+            this.QuitBtn.Text = "Quit";
+            this.QuitBtn.UseVisualStyleBackColor = true;
+            this.QuitBtn.Click += new System.EventHandler(this.QuitBtn_Click);
+            // 
+            // FindingCallNumbers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 445);
+            this.Controls.Add(this.QuitBtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ScoreLbl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.MessageLbl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NextBtn);
+            this.Controls.Add(this.SubmitBtn);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.questionLbl);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "FindingCallNumbers";
+            this.Text = "FindingCallNumbers";
+            this.Load += new System.EventHandler(this.FindingCallNumbers_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label questionLbl;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button SubmitBtn;
+        private System.Windows.Forms.Button NextBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label MessageLbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label ScoreLbl;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button QuitBtn;
+    }
+}
